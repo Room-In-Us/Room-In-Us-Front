dfbf51e85e46a3fc049365356b15db96de11f2a6
ehgus0526 <ehgus0526@naver.com>
2024-09-02 21:32:28 +0900

    Feat: 홈화면 레이아웃 구현 (#1)

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,135 +1,153 @@
+import { useState } from 'react';
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { useDrag } from 'react-use-gesture';
 import LocationCard from '../assets/images/mainPage/locationCard.png';
 import GenreCard from '../assets/images/mainPage/genreCard.png';
 import LevelCard from '../assets/images/mainPage/levelCard.png';
-import LeftLine from '../assets/images/mainPage/cardLeftLine.png';
 import CenterLine from '../assets/images/mainPage/cardCenterLine.png';
-import RightLine from '../assets/images/mainPage/cardRightLine.png';
 
 function MainPage() {
+    // state 관리
+    const [positions, setPositions] = useState(['left', 'center', 'right']);
+
+    // 카드 드래깅 함수
+    const bind = useDrag(({ direction: [dx], down, distance }) => {
+        if (!down && distance > 10) {
+            if (dx > 0) {
+                // 오른쪽으로 드래깅
+                setPositions(([left, center, right]) => [center, right, left]);
+            } else {
+                // 왼쪽으로 드래깅
+                setPositions(([left, center, right]) => [right, left, center]);
+            }
+        }
+    });
+
+    // 기본 드래그 동작 방지 함수 (지구모양 아이콘 없애기)
+    const handleDragStart = (e) => {
+        e.preventDefault();
+    };
+
     // navigate
     const navigate = useNavigate();
 
     return (
         <PageWrapper>
-            <CardWrapper>
-                <StyledSwiper
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={2}
-                    loop={true}
-                    spaceBetween={-133}
-                    effect={'coverflow'}
-                    coverflowEffect={{
-                        rotate: 80,
-                        stretch: -33,
-                        depth: 450,
-                        modifier: 1,
-                        slideShadows: false,
-                    }}
-                    pagination={false}
-                    modules={[EffectCoverflow, Pagination]}
-                >
-                    <StyledSwiperSlide>
-                        <StyledCard src={LocationCard} alt="위치"
-                                    onClick={() => navigate("/location")}/>
-                    </StyledSwiperSlide>
-                    <StyledSwiperSlide>
-                        <StyledCard src={GenreCard} alt="장르"
-                                    onClick={() => navigate("/genre")}/>
-                    </StyledSwiperSlide>
-                    <StyledSwiperSlide>
-                        <StyledCard src={LevelCard} alt="숙련도"
-                                    onClick={() => navigate("/level")}/>
-                    </StyledSwiperSlide>
-                    <StyledSwiperSlide>
-                        <StyledCard src={LocationCard} alt="위치"
-                                    onClick={() => navigate("/location")}/>
-                    </StyledSwiperSlide>
-                    <StyledSwiperSlide>
-                        <StyledCard src={GenreCard} alt="장르"
-                                    onClick={() => navigate("/genre")}/>
-                    </StyledSwiperSlide>
-                    <StyledSwiperSlide>
-                        <StyledCard src={LevelCard} alt="숙련도"
-                                    onClick={() => navigate("/level")}/>
-                    </StyledSwiperSlide>
-                </StyledSwiper>
-
-                {/* 하단 선 */}
-                <CardLineWrapper>
-                    <StyledLeftLine src={LeftLine} alt="좌측선"/>
-                    <StyledCenterLine src={CenterLine} alt="중앙선"/>
-                    <StyledRightLine src={RightLine} alt="우측선"/>
-                </CardLineWrapper>
+            {/* 카드 영역 */}
+            <CardWrapper {...bind()}>
+                <Card className={positions[0]} onClick={() => navigate("/Level")} onDragStart={handleDragStart}>
+                    <StyledImg src={LevelCard} alt="숙련도" />
+                </Card>
+                <Card  className={positions[1]} onClick={() => navigate("/location")} onDragStart={handleDragStart}>
+                    <StyledImg src={LocationCard} alt="위치" />
+                </Card>
+                <Card  className={positions[2]} onClick={() => navigate("/Genre")} onDragStart={handleDragStart}>
+                    <StyledImg src={GenreCard} alt="장르" />
+                </Card>
             </CardWrapper>
+
+            {/* 하단 라인 영역 */}
+            <CardLineWrapper>
+                <StyledCenterLine className={positions[0]} src={CenterLine} alt="하단라인"/>
+                <StyledCenterLine className={positions[1]} src={CenterLine} alt="하단라인"/>
+                <StyledCenterLine className={positions[2]} src={CenterLine} alt="하단라인"/>
+            </CardLineWrapper>
         </PageWrapper>
-    )
-}
+    );
+};
 
 export default MainPage;
 
 // CSS
 const PageWrapper = styled.div`
+    padding-top: 5em;
     display: flex;
+    flex-direction: column;
     justify-content: center;
 `;
 
 const CardWrapper = styled.div`
-    // border: 1px solid red;
-    margin-top: 5em;
-    width: 70em;
-`;
-
-const StyledSwiper = styled(Swiper)`
-    width : 90%;
-`;
-
-const StyledSwiperSlide = styled(SwiperSlide)`
+    position: relative;
+    margin: 0 10%;
+    width: 80%;
+    height: 25em;
     display: flex;
     justify-content: center;
     align-items: center;
-    transition: transform 0.5s ease, opacity 0.5s ease;
-    &.swiper-slide-prev,
-    &.swiper-slide-next {
-        transform: scale(0.8);
-    }
-    &.swiper-slide-active {
-        transform: scale(1);
-        opacity: 1;
-    }
-    cursor: pointer;
+    perspective: 1500px;
 `;
 
-const StyledCard = styled.img`
+const Card = styled.div`
+    position: absolute;
     border-radius: 2em;
-    width: 25.8234em;
-    height: 23.8438em;
+    width: 28.40574em;
+    height: 26.22818em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.5s ease;
+    will-change: transform;
+    backface-visibility: hidden;
+    cursor: pointer;
+
+    &.left {
+        transform: translateX(-23em) scale(0.7) rotateY(70deg);
+        z-index: 1;
+        filter: brightness(70%);
+    }
+
+    &.center {
+        transform: translateX(0) scale(1) rotateY(0deg);
+        z-index: 2;
+    }
+
+    &.right {
+        transform: translateX(23em) scale(0.7) rotateY(-70deg);
+        z-index: 1;
+        filter: brightness(70%);
+    }
+`;
+
+const StyledImg = styled.img`
+    border-radius: 2em;
+    width: 100%;
+    height: 100%;
 `;
 
 const CardLineWrapper = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
+    position: relative;
+    perspective: 1500px;
 `;
 
-const StyledLeftLine = styled.img`
-    margin-bottom: 3em;
-    width: 8.27em;
-    height: 2.4539em;
-`;
 const StyledCenterLine = styled.img`
+    position: absolute;
     margin: 3em 3em 0 3em;
     width: 42.024em;
     height: 2.228em;
-`;
-const StyledRightLine = styled.img`
-    margin-bottom: 3em;
-    width: 8.27em;
-    height: 2.4539em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.5s ease;
+    will-change: transform;
+    backface-visibility: hidden;
+    cursor: pointer;
+
+    &.left {
+        transform: translateX(-28em) scale(0.2) rotate(20deg);
+        z-index: 1;
+    }
+
+    &.center {
+        transform: translateY(3em) scale(1) rotateY(0deg);
+        z-index: 2;
+    }
+
+    &.right {
+        transform: translateX(28em) scale(0.2) rotate(-20deg);
+        z-index: 1;
+    }
 `;
