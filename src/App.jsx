ebd5fec5bd5be8b46f4ece7fdea7629b29729659
ehgus0526 <ehgus0526@naver.com>
2024-09-05 16:56:20 +0900

    Feat: 로그인 페이지 레이아웃 구현 (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import MainLayOut from './layout/MainLayOut';
 import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -13,7 +14,10 @@ function App() {
         <Route path="/" element={<MainPage/>} />
         
         {/* 로그인 페이지 */}
-        <Route path="/login" element={<MainPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+
+        {/* 회원가입 페이지 */}
+        <Route path="/signup" element={<LoginPage/>} />
 
         {/* 위치 페이지 */}
         <Route path="/location" element={<MainPage/>} />
