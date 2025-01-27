a72ce6397ee025fad360420c66bf8d3491f974ab
ehgus0526 <ehgus0526@naver.com>
2025-01-27 18:02:54 +0900

    Feat: 풋터 레이아웃 구현 (#61)

--- src/widgets/MainLayOut.jsx
+++ src/widgets/MainLayOut.jsx
@@ -1,21 +1,30 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../shared/components/Header';
+import Footer from '../shared/components/Footer';
 import styled from 'styled-components';
 
 function MainLayOut() {
   return (
-    <>
+    <MainWrapper>
       <Header />
       <ContentWrapper>
         <Outlet />
       </ContentWrapper>
-    </>
+      <Footer />
+    </MainWrapper>
   );
 }
 
 export default MainLayOut;
 
 // CSS
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
 const ContentWrapper = styled.div`
-  padding-top: 6em;
+  margin-top: 6rem;
+  flex-grow: 1;
 `;
