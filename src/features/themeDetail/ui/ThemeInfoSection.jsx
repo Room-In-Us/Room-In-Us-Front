89dbc8a9f88578a7c934bdf969679c65ae90071f
ehgus0526 <ehgus0526@naver.com>
2025-05-03 20:44:51 +0900

    Feat: 테마상세 페이지 콘텐츠 영역 섹션 구분 (#109)

--- src/features/themeDetail/ui/ThemeInfoSection.jsx
+++ src/features/themeDetail/ui/ThemeInfoSection.jsx
@@ -3,7 +3,45 @@ import styled from "styled-components";
 function ThemeInfoSection() {
   return (
     <ComponentWrapper>
-      
+      {/* 테마 정보 요약 */}
+      <SectionWrapper>
+        <SectionTitle>
+          테마 정보 요약
+        </SectionTitle>
+        <Divider></Divider>
+      </SectionWrapper>
+
+      {/* 테마 스토리 */}
+      <SectionWrapper>
+        <SectionTitle>
+          테마 스토리
+        </SectionTitle>
+        <Divider></Divider>
+      </SectionWrapper>
+
+      {/* 테마 후기 */}
+      <SectionWrapper>
+        <SectionTitle>
+          테마 후기
+        </SectionTitle>
+        <Divider></Divider>
+      </SectionWrapper>
+
+      {/* 인당 가격 안내 */}
+      <SectionWrapper>
+        <SectionTitle>
+          인당 가격 안내
+        </SectionTitle>
+        <Divider></Divider>
+      </SectionWrapper>
+
+      {/* 매장 정보 */}
+      <SectionWrapper>
+        <SectionTitle>
+          매장 정보
+        </SectionTitle>
+        <Divider></Divider>
+      </SectionWrapper>
     </ComponentWrapper>
   )
 }
@@ -18,4 +56,31 @@ const ComponentWrapper = styled.div`
   align-items: flex-start;
   gap: 1.25rem;
   flex-shrink: 0;
+`;
+
+const SectionWrapper = styled.div`
+  border-radius: 0.625rem;
+  padding: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+  align-self: stretch;
+  background: var(--RIU_Monochrome-10, #F9F9FB);
+`;
+
+const SectionTitle = styled.div`
+  color: var(--RIU_Primary-100, #718FF2);
+  text-align: center;
+  font-family: 'Pretendard-Bold';
+  font-size: 1rem;
+  line-height: normal;
+`;
+
+const Divider = styled.hr`
+  border: none;
+  margin: 0;
+  width: 41.25rem;
+  height: 0.0625rem;
+  background: #C4C6D1;
 `;
